eaa29ec4ca51a9d536554bb65f805f412422cf1f
David Lumm <david.lumm@twinklebob.co.uk>
2017-02-06 21:49:55 +0000

    Code cleanup

--- action.php
+++ action.php
@@ -16,48 +16,44 @@
  */
 
 // must be run within Dokuwiki
-if (!defined('DOKU_INC')) die();
+if (!defined('DOKU_INC')) {
+    die();
+}
 
-if (!defined('DOKU_LF')) define('DOKU_LF', "\n");
-if (!defined('DOKU_TAB')) define('DOKU_TAB', "\t");
-if (!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
+if (!defined('DOKU_LF')) {
+    define('DOKU_LF', "\n");
+}
+if (!defined('DOKU_TAB')) {
+    define('DOKU_TAB', "\t");
+}
+if (!defined('DOKU_PLUGIN')) {
+    define('DOKU_PLUGIN', DOKU_INC . 'lib/plugins/');
+}
 
-require_once DOKU_PLUGIN.'action.php';
+require_once DOKU_PLUGIN . 'action.php';
 
 class action_plugin_glip extends DokuWiki_Action_Plugin {
 
     function register(Doku_Event_Handler $controller) {
-       $controller->register_hook('ACTION_ACT_PREPROCESS', 'BEFORE', $this, 'handle_action_act_preprocess');
+        $controller->register_hook('ACTION_ACT_PREPROCESS', 'BEFORE', $this, 'handle_action_act_preprocess');
     }
 
-    function handle_action_act_preprocess(Doku_Event $event, $param) {
-		file_put_contents('php://stderr', 'In handle_action_act_preprocess' . PHP_EOL);
-		file_put_contents('php://stderr', print_r($event->data, true) . PHP_EOL);
+    function handle_action_act_preprocess(Doku_Event $event) {
         if (isset($event->data['save'])) {
-			
-            //if ($event->data['save'] == 'Save') {
-				file_put_contents('php://stderr', 'Is save' . PHP_EOL);
-                $this->handle();
-            //}
+            $this->handle();
         }
         return;
     }
 
     private function handle() {
-		file_put_contents('php://stderr', 'In handle' . PHP_EOL);
         global $SUM;
         global $INFO;
 
         $fullname = $INFO['userinfo']['name'];
         $username = $INFO['client'];
-        $page     = $INFO['namespace'] . $INFO['id'];
-        $summary  = $SUM;
-        $minor    = (boolean) $_REQUEST['minor'];
-
-        $config = array(
-                'url'       => $this->getConf('glip_url'),
-				'icon_url'	=> $this->getConf('glip_icon_url'),
-                'activity'  => $this->getConf('glip_activity'));
+        $page = $INFO['namespace'] . $INFO['id'];
+        $summary = $SUM;
+        $minor = (boolean) $_REQUEST['minor'];
 
         /* Namespace filter */
         $ns = $this->getConf('glip_namespaces');
@@ -69,37 +65,40 @@ class action_plugin_glip extends DokuWiki_Action_Plugin {
             }
         }
 
-		$say = '**' . $fullname . '** updated the Wikipage **[' . $INFO['id'] . '](' . $this->urlize() . ')**';
-        if ($minor) $say = $say . ' [minor edit]';
-        if ($summary) $say = $say . PHP_EOL . '*' . $summary . '*';
+        $say = '**' . $fullname . '** updated the Wikipage **[' . $INFO['id'] . '](' . $this->urlize() . ')**';
+        if ($minor) {
+            $say = $say . ' [minor edit]';
+        }
+        if ($summary) {
+            $say = $say . PHP_EOL . 'Edit summary: *' . $summary . '*';
+        }
 
         //error_log($say);
         //error_log(json_encode($config));
 
-		$data = array(
-			"icon" 		=> $this->getConf('glip_icon_url'),
-			"activity"	=> $this->getConf('glip_activity'),
-			"body"		=> $say
-		);
-        
+        $data = array(
+            "icon" => $this->getConf('glip_icon_url'),
+            "activity" => $this->getConf('glip_activity'),
+            "body" => $say
+        );
+
         $data_string = json_encode($data);
-        
+
         $http = new DokuHTTPClient();
         $http->headers['content-type'] = 'application/json';
         $response = $http->post($this->getConf('glip_url'), $data_string);
-        if($response === false) {
+        if ($response === false) {
             error_log("Error notifying Glip: " . $http->error);
         }
     }
 
     /* Make our URLs! */
     private function urlize() {
-
         global $INFO;
         global $conf;
         $page = $INFO['id'];
 
-        switch($conf['userewrite']) {
+        switch ($conf['userewrite']) {
             case 0:
                 $url = DOKU_URL . "doku.php?id=" . $page;
                 break;
@@ -118,6 +117,7 @@ class action_plugin_glip extends DokuWiki_Action_Plugin {
         }
         return $url;
     }
+
 }
 
 // vim:ts=4:sw=4:et:enc=utf-8:
